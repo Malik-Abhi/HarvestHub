6727adb850e0a833ebce64f735eeb23340bebcf8
Aryan <aryan.m@froogal.ai>
2025-08-28 08:23:55 +0530

    added backend url

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:8000/api';
+const API_URL = 'https://harvesthub-hyge.onrender.com/';
 
 // Reusable API call function
 const apiRequest = async (endpoint, method = 'GET', body = null, token = null) => {
