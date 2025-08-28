22dd82b8ab392d0ed7b1376b439136af02c99b48
Aryan <aryan.m@froogal.ai>
2025-08-28 10:04:15 +0530

    change CORS options

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: 'https://harvest-hub-rosy.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
